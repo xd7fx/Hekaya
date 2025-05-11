679933d063ddb66e585be6a58245951a99411eaa
Abdulrahman Alnashri <d7.fx99@gmail.com>
2025-05-11 13:43:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,14 @@ if input_method == "📁 رفع صورة":
     uploaded_file = st.file_uploader("ارفع صورة", type=["jpg", "jpeg", "png"])
 elif input_method == "📸 كاميرا":
     uploaded_file = st.camera_input("التقط صورة بالكاميرا")
+    if uploaded_file:
+        # تدوير الصورة 90 درجة لتكون بالعرض
+        image = Image.open(uploaded_file).convert("RGB")
+        image = image.transpose(method=Image.Transpose.ROTATE_270)
+        # إعادة حفظ الصورة في uploaded_file لتُستخدم لاحقًا
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_img:
+            image.save(temp_img.name)
+            uploaded_file = open(temp_img.name, "rb")
 
 # ✅ معالجة صورة ثابتة
 if uploaded_file is not None:
